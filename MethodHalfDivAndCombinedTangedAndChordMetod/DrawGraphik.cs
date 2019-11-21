fbac32a68675bfbfe8b854d74eedee83ee2e18c1
Maks <klavioturasucka2@gmail.com>
2019-11-21 13:41:22 +0900

    MethodHalfDivAndCombinedTangedAndChordMetod-0.1

--- MethodHalfDivAndCombinedTangedAndChordMetod/DrawGraphik.cs
+++ MethodHalfDivAndCombinedTangedAndChordMetod/DrawGraphik.cs
@@ -21,7 +21,7 @@ namespace MethodHalfDivAndCombinedTangedAndChordMetod
         {
             Bitmap bitmap = new Bitmap(Width + Left, Height + Top);
             Graphics graph = Graphics.FromImage(bitmap);
-            Pen myPen = new Pen(Color.Black, 1);
+            Pen myPen = new Pen(Color.DimGray, 0.1f);
 
             for (int i = Top; i <= Height + Top; i += Height / 20)
             {
@@ -32,7 +32,7 @@ namespace MethodHalfDivAndCombinedTangedAndChordMetod
                 graph.DrawLine(myPen, i, Top, i, Height + Top);
             }
 
-            myPen.Width = 2;
+            myPen.Color = Color.Black;
             graph.DrawLine(myPen, (Width / 2) + Left, Top, (Width / 2) + Left, Height + Top);
             graph.DrawLine(myPen, Left, (Height / 2) + Top, Width + Left, (Height / 2) + Top);
 
@@ -46,14 +46,40 @@ namespace MethodHalfDivAndCombinedTangedAndChordMetod
         /// <param name="Left">Расстояние слево координатной плоскости от левого верхнего угла окна</param>
         /// <param name="Height">Высота координатной плоскости</param>
         /// <param name="Width">Ширина координатной плоскости</param>
-        public static Bitmap Graphix(ushort Top, ushort Left, ushort Height, ushort Width)
+        public static Bitmap CombienMethodChordAndTanged(ushort Top, ushort Left, ushort Height, ushort Width)
         {
-            Bitmap bitmap = new Bitmap(Width + Left, Height + Top);
-            Graphics graph = Graphics.FromImage(bitmap);
-            Pen myPen = new Pen(Color.Black, 1);
+            CoordinatePlane(Top, Left, Height, Width);
 
+            Bitmap bitmap = CoordinatePlane(Top, Left, Height, Width); 
+            Graphics graph = Graphics.FromImage(bitmap);
+            Pen myPen = new Pen(Color.Black, 0.5f);
+
+            float x0 = (Width / -40),
+                  y0 = Equation(x0),
+                  x = x0,
+                  y = 0;
+
+            for (int i = 0; i < Width; i++)
+            {
+                x += 0.05f;
+                y = Equation(x);
+
+                graph.DrawLine(myPen, Left + ((Width / 40) + x0) * 20, 
+                                      Height / 2 - y0 + Top, 
+                                      Left + ((Width / 40) + x) * 20, 
+                                      Height / 2 - y + Top);
+
+                x0 = x;
+                y0 = y;
+            }
 
             return bitmap;
+        }
+
+        public static float Equation(float x0)
+        {
+            return (float)((3 * x0) + Math.Pow(5, x0) + 6);
+            //return 4 * x0 * x0 * x0 - 5 * x0 * x0 - 2 * x0 + 2;
         }
     }
 }
